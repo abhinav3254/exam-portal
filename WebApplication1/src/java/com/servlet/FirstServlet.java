d7ecc748bfa260441ef932d728b6f6c335f8cd75
abhinav3254 <abhinav3254@gmail.com>
2022-10-13 10:02:52 +0530

    Non-life cycle method

--- WebApplication1/src/java/com/servlet/FirstServlet.java
+++ WebApplication1/src/java/com/servlet/FirstServlet.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.servlet;
 
-/**
- *
- * @author abhin
- */
-public class FirstServlet {
+import java.io.IOException;
+import javax.servlet.*;
+public class FirstServlet implements Servlet{
+//    Life cycle methods
     
+    ServletConfig conf;
+    
+    @Override
+    public void init(ServletConfig config) {
+        this.conf = config;
+        System.out.println("creatting the object .....");
+    }
+    @Override
+    public void service (ServletRequest req , ServletResponse res)  throws ServletException, IOException {
+        System.out.println("Servicing.........................");
+    }
+    
+    @Override
+    public void destroy() {
+        System.out.println("Going to destroy servlet object");
+    }
+    
+//    Non life cycle method
+    @Override
+    public ServletConfig getServletConfig () {
+    return this.conf; 
+          }
+    @Override
+    public String getServletInfo() {
+    return "this servlet created by abhinav Jha";
+    }    
 }
