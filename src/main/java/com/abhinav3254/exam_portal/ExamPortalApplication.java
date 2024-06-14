38f73d59760094fb3bef911401caa108a144ba03
abhinav3254 <abhinav3254@gmail.com>
2024-06-14 22:51:56 +0530

    Java version changed

---
+++ src/main/java/com/abhinav3254/exam_portal/ExamPortalApplication.java
@@ -0,0 +1,13 @@
+package com.abhinav3254.exam_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhinav3254/exam_portal/ExamPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav3254.exam_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
